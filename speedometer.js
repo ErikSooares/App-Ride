35020fa8f6dd454adfb06a20e4479560805df882
Erik Soares <eriksoares1678@gmail.com>
2024-07-11 05:23:47 -0300

    first commit

--- speedometer.js
+++ speedometer.js
@@ -3,6 +3,25 @@ const btnStart = document.querySelector("#btnStart");
 const btnStop = document.querySelector("#btnStop");
 
 btnStart.addEventListener("click", ()=>{
+
+    function handleSuccess(position){
+        if(position.coords.speed == null){
+            speedElement.innerText = 0;
+        }
+        else{
+            speedElement.innerText = parseInt(position.coords.speed) * 3.6;
+        }
+        console.log(position)
+    }
+
+    function handleError(position){
+        console.log(error.msg)
+    }
+
+    const options = {enableHighAccuracy: true}
+
+    navigator.geolocation.watchPosition(handleSuccess, handleError, options)
+
     btnStart.classList.add("d-none");
     btnStop.classList.remove("d-none");
 })
