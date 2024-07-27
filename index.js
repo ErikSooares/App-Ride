63e7f19b137e5db2326b404f38f3dc9549d27c0e
Erik Soares <eriksoares1678@gmail.com>
2024-07-27 12:56:23 -0300

    update

--- index.js
+++ index.js
@@ -13,12 +13,23 @@ allRides.forEach(async ([id, value]) => {
 
     const itemElement = document.createElement("li");
     itemElement.id = ride.id;
+    itemElement.className = "d-flex p-1 align-items-center justify-content-between shadow-sm gap-3"
+
+    const mapElement = document.createElement("div")
+    mapElement.style = "width: 100px; height: 100px";
+    mapElement.classList.add("bg-secondary")
+    mapElement.classList.add("rounded-4")
+
+    const dataElement = document.createElement("div")
+    dataElement.className = "flex-fill d-flex flex-column"
 
     const cityDiv = document.createElement("div");
     cityDiv.innerText = `${firstLocationData.city} - ${firstLocationData.countryName}`;
+    cityDiv.className = "text-primary mb-2"
 
     const maxSpeedDiv = document.createElement("div");
     maxSpeedDiv.innerText = `Max speed: ${getMaxSpeed(ride.data)} km/h`
+    maxSpeedDiv.className = "h5"
 
     const distanceDiv = document.createElement("div")
     distanceDiv.innerText = `Distance: ${getDistance(ride.data)} km`
@@ -28,12 +39,16 @@ allRides.forEach(async ([id, value]) => {
 
     const dateDiv = document.createElement("div")
     dateDiv.innerText = `${getStartDate(ride)}`
+    dateDiv.className = "text-secondary mt-2"
 
-    itemElement.appendChild(cityDiv)
-    itemElement.appendChild(maxSpeedDiv)
-    itemElement.appendChild(distanceDiv)
-    itemElement.appendChild(durationDiv)
-    itemElement.appendChild(dateDiv)
+    dataElement.appendChild(cityDiv)
+    dataElement.appendChild(maxSpeedDiv)
+    dataElement.appendChild(distanceDiv)
+    dataElement.appendChild(durationDiv)
+    dataElement.appendChild(dateDiv)
+
+    itemElement.appendChild(mapElement)
+    itemElement.appendChild(dataElement)
 
     rideListELement.appendChild(itemElement)
 })
